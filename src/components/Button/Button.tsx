68153d5848bc327c5963c9e8d09b5fdeee07b4b5
Mehdi Mahmoudi <mahmoudi.web.dev@gmail.com>
2023-05-08 22:08:28 +0330

    refactor: cleanup button.tsx

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import Image from 'next/image';
 
-interface Propsbutton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface Propsbutton {
   children: React.ReactNode;
-  image: any;
 }
 
-const BlueButton: React.FC<Propsbutton> = ({
-  children,
-  image,
-  ...attributes
-}) => {
+const BlueButton: React.FC<Propsbutton> = ({ children }) => {
   return (
     <div className='flex  flex-col items-center'>
-      <button
-        {...attributes}
-        className='bg-warm-blue w-44 text-white py-3 px-6 rounded-xl flex flex-row items-center gap-5'
-      >
+      <button className='bg-warm-blue w-44 text-white py-3 px-6 rounded-xl flex flex-row items-center gap-5'>
         {children}
-        <Image src={image} width={24} height={24} alt='' />
       </button>
     </div>
   );
