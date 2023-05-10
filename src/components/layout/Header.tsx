b206c697a64ecf03a4e694219e1fc78f1c837892
Mehdi Mahmoudi <mahmoudi.web.dev@gmail.com>
2023-05-10 09:16:12 +0330

    feat: create Header component

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className='text-center'>
+      <p className='text-warm-blue font-bold pb-2'>مرحله 1 از 3</p>
+      <p className=' text-black  font-bold text-xl'>
+        لطفا اطلاعات خود را با دقت وارد نمائید
+      </p>
+    </div>
+  );
+};
+
+export default Header;
