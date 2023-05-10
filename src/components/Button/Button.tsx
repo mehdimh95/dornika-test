b14637d4ad378187f040559e55e31ec2a5a4b437
Mehdi Mahmoudi <mahmoudi.web.dev@gmail.com>
2023-05-10 09:26:32 +0330

    refactor: Button.tsx flex

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,7 +7,7 @@ interface Propsbutton {
 const BlueButton: React.FC<Propsbutton> = ({ children }) => {
   return (
     <div className='flex  flex-col items-center'>
-      <button className='bg-warm-blue w-44 text-white py-3 px-6 rounded-xl flex flex-row items-center gap-5'>
+      <button className='bg-warm-blue w-44 text-white py-3 px-6 rounded-xl flex flex-row items-center justify-center gap-5'>
         {children}
       </button>
     </div>
