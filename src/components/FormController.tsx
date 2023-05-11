96aa9a2657a7dc7a535e035f5f0cfc6625235733
Mehdi Mahmoudi <mahmoudi.web.dev@gmail.com>
2023-05-11 12:16:06 +0330

    build: add react-leaflet

--- src/components/FormController.tsx
+++ src/components/FormController.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-interface Props {
-  children: React.ReactNode;
+
+interface IFormController extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
-  placeholder: string;
+  icon?: React.ReactNode;
 }
 
-const FormController: React.FC<Props> = ({ label, placeholder, children }) => {
+const FormController = ({
+  label,
+  name,
+  icon,
+  ...inputProps
+}: IFormController) => {
   return (
     <div className='flex flex-col relative'>
-      <label className='label'>
+      <label className='label' htmlFor={name}>
         <span className='label-text text-base text-slate-600  font-bold absolute top-[-15px] right-9 bg-white px-2'>
           {label}
         </span>
       </label>
       <input
-        type='text'
-        placeholder={placeholder}
         className='input border border-fade-gray leading-[60px] w-full px-20 rounded-full outline-fade-gray placeholder:text-fade-gray'
+        {...inputProps}
       />
       <div className='absolute top-1/2 -translate-y-1/2 right-4 border-l border-fade-gray box-content pl-4'>
-        {children}
+        {icon}
       </div>
     </div>
   );
