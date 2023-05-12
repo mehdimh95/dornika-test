666863f00a1ed2d37b5021c11106b941696b5ee3
Mehdi Mahmoudi <mahmoudi.web.dev@gmail.com>
2023-05-12 15:02:44 +0330

    feat: add steps to aside

--- src/components/layout/Aside.tsx
+++ src/components/layout/Aside.tsx
@@ -1,4 +1,12 @@
 import Image from 'next/image';
+import Steps, { IStep } from '../Steps';
+
+const stepItems: IStep[] = [
+  { id: 1, label: 'مرحله 1' },
+  { id: 2, label: 'مرحله 2' },
+  { id: 3, label: 'مرحله 3' },
+];
+
 const Aside = () => {
   return (
     <div className='w-1/4 flex flex-col  justify-start items-center bg-warm-blue rounded-r-2xl'>
@@ -10,6 +18,7 @@ const Aside = () => {
         className='pt-20 pb-5'
       />
       <p className=' text-white font-black text-4xl'>ثبت نام</p>
+      <Steps currentStepId={2} steps={stepItems} />
     </div>
   );
 };
