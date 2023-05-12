c83a24348dd313f4a6abcd1d2314a3ab4f32753a
Mehdi Mahmoudi <mahmoudi.web.dev@gmail.com>
2023-05-12 13:44:18 +0330

    refactor: delete container

--- src/components/layout/Container.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Container: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return (
-    <div className='flex h-screen items-stretch py-8 p-24'>{children}</div>
-  );
-};
-
-export default Container;
