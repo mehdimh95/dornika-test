95e2d12e30db22427d1ddc91a9f1fc711bdd748f
Mehdi Mahmoudi <mahmoudi.web.dev@gmail.com>
2023-05-12 13:52:09 +0330

    feat: create formcontroller

--- src/components/FormController.tsx
+++ src/components/FormController.tsx
@@ -3,30 +3,41 @@ import React from 'react';
 interface IFormController extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
   icon?: React.ReactNode;
+  error?: string;
 }
 
-const FormController = ({
-  label,
-  name,
-  icon,
-  ...inputProps
-}: IFormController) => {
-  return (
-    <div className='flex flex-col relative'>
-      <label className='label' htmlFor={name}>
-        <span className='label-text text-base text-slate-600  font-bold absolute top-[-15px] right-9 bg-white px-2'>
-          {label}
+const FormController = React.forwardRef<HTMLInputElement, IFormController>(
+  ({ label, name, icon, error, ...inputProps }, ref) => {
+    return (
+      <div className='flex flex-col gap-3'>
+        <div className=' relative'>
+          <label className='label' htmlFor={name}>
+            <span className='label-text text-base text-slate-600  font-bold absolute top-[-15px] right-9 bg-white px-2'>
+              {label}
+            </span>
+          </label>
+          <input
+            id={name}
+            name={name}
+            ref={ref}
+            {...inputProps}
+            className='input border focus:outline-red-600 focus:bg-white outline outline-1 border-fade-gray leading-[60px] w-full px-20 rounded-full outline-fade-gray placeholder:text-fade-gray'
+          />
+
+          <div className='absolute top-1/2 -translate-y-1/2 right-4 border-l border-fade-gray box-content pl-4'>
+            {icon}
+          </div>
+        </div>
+        <span>
+          {error && (
+            <label className='label'>
+              <span className='label-text-alt text-red-600'>{error}</span>
+            </label>
+          )}
         </span>
-      </label>
-      <input
-        className='input border border-fade-gray leading-[60px] w-full px-20 rounded-full outline-fade-gray placeholder:text-fade-gray'
-        {...inputProps}
-      />
-      <div className='absolute top-1/2 -translate-y-1/2 right-4 border-l border-fade-gray box-content pl-4'>
-        {icon}
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default FormController;
