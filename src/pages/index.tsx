4b7c8f330a5c2b76a29096ee025ab0d689ab7f9d
Mehdi Mahmoudi <mahmoudi.web.dev@gmail.com>
2023-05-18 23:58:28 +0330

    feat: add trenditem type

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,6 +21,21 @@ const AdvancedRealTimeChartNoSSR = dynamic(
   }
 );
 
+type TTrendItem = {
+  item: {
+    id: string;
+    coin_id: 4128;
+    name: string;
+    symbol: string;
+    market_cap_rank: 10;
+    thumb: string;
+    small: string;
+    large: string;
+    slug: string;
+    price_btc: 0.0007640637388540485;
+    score: 0;
+  };
+};
 const Dashboard = () => {
   return (
     <Layout>
