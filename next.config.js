b88a3b7f5cf0133ac1a824e0723ab58728f4d3c1
Mehdi Mahmoudi <mahmoudi.web.dev@gmail.com>
2023-05-20 20:50:40 +0330

    chore: update image domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['assets.coingecko.com'],
+    domains: ['assets.coingecko.com', 'picsum.photos'],
   },
   eslint: {
     'react/display-name': true,
